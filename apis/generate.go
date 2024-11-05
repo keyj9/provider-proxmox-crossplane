1be52cd507fc532a92f2ab1e1b0deea2aa491b6e
apple <apple@apples-MacBook-Air-m1.local>
2024-11-05 19:20:16 +0100

    Add the SKIP_GENERATE variable

--- apis/generate.go
+++ apis/generate.go
@@ -5,30 +5,23 @@
 Copyright 2021 Upbound Inc.
 */
 
-// NOTE: See the below link for details on what is happening here.
-// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+// NOTE: All generation steps are commented out as we're using pre-generated files
 
 // Remove existing CRDs
-//go:generate rm -rf ../package/crds
+// //go:generate rm -rf ../package/crds
 
 // Remove generated files
-//go:generate bash -c "find . -iname 'zz_*' ! -iname 'zz_generated.managed*.go' -delete"
-//go:generate bash -c "find . -type d -empty -delete"
-//go:generate bash -c "find ../internal/controller -iname 'zz_*' -delete"
-//go:generate bash -c "find ../internal/controller -type d -empty -delete"
-//go:generate rm -rf ../examples-generated
+// //go:generate bash -c "find . -iname 'zz_*' ! -iname 'zz_generated.managed*.go' -delete"
+// //go:generate bash -c "find . -type d -empty -delete"
+// //go:generate bash -c "find ../internal/controller -iname 'zz_*' -delete"
+// //go:generate bash -c "find ../internal/controller -type d -empty -delete"
+// //go:generate rm -rf ../examples-generated
 
 // Temporarily disabled scraper
 // //go:generate go run github.com/crossplane/upjet/cmd/scraper -n ${TERRAFORM_PROVIDER_SOURCE} -r ../.work/${TERRAFORM_PROVIDER_SOURCE}/${TERRAFORM_DOCS_PATH} -o ../config/provider-metadata.yaml
 
 // Run Upjet generator
-//go:generate go run ../cmd/generator/main.go ..
-
-// Generate deepcopy methodsets and CRD manifests
-//go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../hack/boilerplate.go.txt paths=./... crd:allowDangerousTypes=true,crdVersions=v1 output:artifacts:config=../package/crds
-
-// Generate crossplane-runtime methodsets (resource.Claim, etc)
-//go:generate go run -tags generate github.com/crossplane/crossplane-tools/cmd/angryjet generate-methodsets --header-file=../hack/boilerplate.go.txt ./...
+// //go:generate go run ../cmd/generator/main.go ..
 
 package apis
 
